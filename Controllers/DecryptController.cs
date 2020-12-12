4f885f22a693aee1d24d1163a6d90d368275e9b4
Ivliev Maxim <muss123balakovo@gmail.com>
2020-12-12 04:28:14 +0300

    Renamed main conyroller, added second decryption. Added pepe background and base form without functions

---
+++ Controllers/DecryptController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryptoService.Controllers
+{
+    public class DecryptController : Controller
+    {
+        // GET: Decrypt/Default
+        public ActionResult Default()
+        {
+            return View();
+        }
+        // GET: Decrypt/Advanced
+        public ActionResult Advanced()
+        {
+            return View();
+        }
+        // GET: Decrypt/Help
+        public ActionResult Help()
+        {
+            return View();
+        }
+
+    }
+}
